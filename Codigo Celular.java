2d274e4d642f67e019206291f1bcaf116f8748cc
Natan Pena <natscoisaseria@gmail.com>
2025-09-10 10:25:34 -0300

    Create Codigo Celular.java

---
+++ Codigo Celular.java
@@ -0,0 +1,22 @@
+class Celular {
+    String marca;
+    int gigabytes;
+    int ram;
+    String processador;
+
+    // Construtor: define todas as características do celular
+    public Celular(String marca, int gigabytes, int ram, String processador) {
+        this.marca = marca;
+        this.gigabytes = gigabytes;
+        this.ram = ram;
+        this.processador = processador;
+    }
+
+       public static void main(String[] args) {
+          Celular p = new Celular("Samsung", 64,"Natan", 4, 2);
+          System.out.println("Marca: " + p.marca);
+          System.out.println("Gigabytes: " + p.valor);
+          System.out.println("Ram: " + p.proprietario);
+          System.out.println("Processador: " + p.processador);
+    }
+}
