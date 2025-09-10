5ed61133a00b01941396deb0a9822aadd5a1104f
Natan Pena <natscoisaseria@gmail.com>
2025-09-10 09:21:28 -0300

    Create ControleRemoto.java

---
+++ ControleRemoto.java
@@ -0,0 +1,39 @@
+class ControleRemoto {
+    String marca;
+    int pilhas;
+    private boolean ligado;
+
+    // Construtor: define a marca e o número de pilhas
+    public ControleRemoto(String marca, int pilhas) {
+        this.marca = marca;
+        this.pilhas = pilhas;
+        this.ligado = false; // Começa sempre desligado
+    }
+
+    // Método para ligar o controle
+    public void ligar() {
+        if (!this.ligado) {
+            this.ligado = true;
+            System.out.println("O controle remoto " + this.marca + " foi ligado.");
+        } else {
+            System.out.println("O controle já está ligado.");
+        }
+    }
+
+    // Método para desligar o controle
+    public void desligar() {
+        if (this.ligado) {
+            this.ligado = false;
+            System.out.println("O controle remoto " + this.marca + " foi desligado.");
+        } else {
+            System.out.println("O controle já está desligado.");
+        }
+    }
+
+     public static void main(String[] args) {
+            ControleRemoto p = new ControleRemoto("Philipss", "Duracell");
+            System.out.println("Marca: " + p.marca);
+            System.out.println("Pilhas: " + p.pilhas);
+    }
+  
+}
