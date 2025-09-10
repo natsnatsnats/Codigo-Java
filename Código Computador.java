830f69915887f33eddf16d98809c2644f306aad2
Natan Pena <natscoisaseria@gmail.com>
2025-09-10 10:36:33 -0300

    Create Código Computador.java

---
+++ Código Computador.java
@@ -0,0 +1,26 @@
+class Computador {
+    int ram;
+    String processador;
+    String placaDeVideo;
+    String monitor;
+    String gabinete;
+
+    // Construtor: define todas as peças do computador
+    public Computador(int ram, String processador, String placaDeVideo, String monitor, String gabinete) {
+        this.ram = ram;
+        this.processador = processador;
+        this.placaDeVideo = placaDeVideo;
+        this.monitor = monitor;
+        this.gabinete = gabinete;
+    }
+
+    public static void main(String[] args) {
+        Computador p = new Computador(8, "Intel core 7", "Nvidia 2090", "LG 24MS500", "Tgt Legion");
+        System.out.println("Ram: " + p.ram);
+        System.out.println("Processador: " + p.processador);
+        System.out.println("Placa de Vídeo: " + p.placaDeVideo);
+        System.out.println("Monitor: " + p.monitor);
+        System.out.println("Gabinete: " + p.gabinete);
+    }
+  
+}
